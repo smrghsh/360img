9a0fafa8beabdb1c26fa62c23ce9979cea5cbb9b
Samir Ghosh <hello@samir.tech>
2023-04-17 00:33:40 -0700

    test: warped scry

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -27,7 +27,7 @@ export default class World
         // const texture = new THREE.VideoTexture( video );
 
         // CHANGE THIS SOURCE HERE!!!!!
-        const imageSource = './images/test.png'
+        const imageSource = './images/test2-warp-transformed.jpg'
         const image = new Image()
         const texture = new THREE.Texture(image)
         image.addEventListener('load', () =>
